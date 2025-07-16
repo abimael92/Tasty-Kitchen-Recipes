a23a040e8e3606113e6ce2950711fd11b492e6f6
Abimael <abimael1992g@gmail.com>
2025-07-15 18:53:17 -0600

    📌 Refactor InteractiveRating component print visibility

--- src/components/InteractiveRating.jsx
+++ src/components/InteractiveRating.jsx
@@ -1,18 +1,21 @@
-import { createSignal } from 'solid-js';
-import RatingStars from './RatingStars';
+// InteractiveRating.jsx
+import React, { useState } from 'react';
+import { SimpleRating } from './SimpleRating.jsx';
 
 export default function InteractiveRating() {
-	const [rating, setRating] = createSignal(0);
+	const [rating, setRating] = useState(0);
+
+	const handleRatingChange = (newRating) => {
+		setRating(newRating);
+		console.log('User rated:', newRating);
+		// Optionally: send newRating to your server here
+	};
 
 	return (
-		<div>
-			<RatingStars
-				onRate={(newRating) => {
-					console.log('New rating:', newRating);
-					setRating(newRating);
-				}}
-			/>
-			<p>Current rating: {rating()}</p>
-		</div>
+		<SimpleRating
+			totalratings={5}
+			defaultRating={rating}
+			getCurrentRatings={handleRatingChange}
+		/>
 	);
 }
