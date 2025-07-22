2bbad969d6c909235aac1f20bf62dec82764dd4a
Abimael <abimael1992g@gmail.com>
2025-07-21 18:40:32 -0600

    🔧 Modify schema index

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -3,6 +3,15 @@ import user from './user'
 import recipeRating from './recipeRating'
 import collection from './collection'
 import savedRecipe from './savedRecipe'
+import groceryList from './groceryList'
 import {eventType} from './eventType'
 
-export const schemaTypes = [recipe, user, recipeRating, collection, savedRecipe, eventType]
+export const schemaTypes = [
+  recipe,
+  user,
+  recipeRating,
+  collection,
+  groceryList,
+  savedRecipe,
+  eventType,
+]
