8ad9f494c53acbe80bb4eff3f36e0ce2376fd84a
Abimael Garcia <abimael1992g@gmail.com>
2025-08-15 17:24:50 -0600

    🔤 Add TextFilter component

---
+++ src/components/filters/TextFilter.jsx
@@ -0,0 +1,11 @@
+export default function TextFilter({ value, onChange }) {
+	return (
+		<input
+			type='text'
+			placeholder='Search by recipe name...'
+			value={value}
+			onInput={(e) => onChange(e.target.value)}
+			className='filter-input'
+		/>
+	);
+}
