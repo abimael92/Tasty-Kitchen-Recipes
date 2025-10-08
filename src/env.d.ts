a6ffb29470939928e4a744f493e38910dbe799e3
Abimael Garcia <abimael1992g@gmail.com>
2025-10-08 12:21:30 -0600

    chore: add environment type declarations

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,5 +6,5 @@ interface ImportMetaEnv {
   readonly SANITY_API_VERSION: string;
   readonly SANITY_USE_CDN: string;
   readonly SANITY_API_TOKEN?: string;
-  readonly MONGO_URI?: string; // Only if actually needed
+  readonly MONGO_URI?: string; 
 }
